68600b27c0d3a80355254da64295149d2db80528
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-11-15 09:29:45 -0700

    Adding Extension Method

---
+++ src/ChinookSolution/ChinookSystem/ChinookSystemBackendDependencies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+#endregion
+
+
+namespace ChinookSystem
+{
+    public static class ChinookExtensions
+    {
+        public static void ChinookSystemBackendDependencies(this IServiceCollection services,
+            Action<DbContextOptionsBuilder> options)
+        {
+            //  Register the DBContext class in CHinook with the service collection
+            services.AddDbContext<Chinook2018Context>(options);
+
+            //  Add any services that you create in the class library
+            //  using.AddTransident<T>(...)
+        }
+    }
+}
--- src/ChinookSolution/WebApp/Program.cs
+++ src/ChinookSolution/WebApp/Program.cs
@@ -1,13 +1,39 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
+using ChinookSystem;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//given
+//  Supplied database connection due to the fact that we create this
+//      web app to use Individual Account
+//  Code retrieves the connection string from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//added
+//  Code retrieves the ChinookDB connection string
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+//  given
+//  Register the supplied connection string with the IServiceCollection (.Services)
+//  Registers the connection string for Individual Accounts
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//  added
+//  Code the logic to add our class library services to IServiceCollection
+//  One could to the registration code here in Program.cs
+//  HOWEVER, every time a service class is added, you would be changing this file
+//  The implementation of the DBContent and AddTransient(..) code in this example
+//      will be done in an extension method to  IServiceCollection
+//  The extension method will be coded inside the ChinookSystem class library
+//  The extension method will have a parameter:  options.UseSqlServer()
+builder.Services.ChinookSystemBackendDependencies(options =>
+    options.UseSqlServer(connectionStringChinook));
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
