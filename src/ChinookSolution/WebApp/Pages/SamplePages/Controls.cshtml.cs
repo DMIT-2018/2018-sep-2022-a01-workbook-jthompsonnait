3e924e353fecb217f1e36af4b27e34c299e7ea79
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-11-15 08:56:39 -0700

    List and Sliders

--- src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -30,7 +30,21 @@ namespace WebApp.Pages.SamplePages
         [BindProperty]
         public string MessageBody { get; set; }
 
+        [BindProperty]
+        public int MyRide { get; set; }
 
+        //  pretend tat the following collection is data from a database
+        //  The collection is based on a 2 property class called SelectionList
+        //  The data for the list will be created in a separate method.
+        public List<SelectionList> Rides { get; set; }
+
+        [BindProperty]
+        public string VacationSpot { get; set; }
+
+        public List<string> VacationSpots { get; set; }
+
+        [BindProperty]
+        public int ReviewRating { get; set; }
         public IActionResult OnPostTextBox()
         {
             Feedback = $"Email {EmailText}; Password {PasswordText}; Date {DateText.ToShortDateString()}; Time {TimeText}";
@@ -42,12 +56,45 @@ namespace WebApp.Pages.SamplePages
             return Page();
         }
 
+        public IActionResult OnPostListSlider()
+        {
+            Feedback = $"Ride {MyRide}; Vacation {VacationSpot}; Review Rating {ReviewRating}";
+            PopulatedList();
+            return Page();
+        }
+
 
 
 
 
         public void OnGet()
         {
+            PopulatedList();
+        }
+
+        private void PopulatedList()
+        {
+            int i = 1;
+            //  Create a pretend collection from the database represents different types
+            //      of transportation (rides)
+            Rides = new List<SelectionList>();
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Car" });
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Bus" });
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Bike" });
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Motorcycle" });
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Boat" });
+            Rides.Add(new SelectionList() { ValueID = i++, DisplayText = "Plane" });
+            Rides.Sort((x,y) => x.DisplayText.CompareTo(y.DisplayText));
+
+            VacationSpots = new List<string>();
+            VacationSpots.Add("California");
+            VacationSpots.Add("Caribbean");
+            VacationSpots.Add("Cruising");
+            VacationSpots.Add("Europe");
+            VacationSpots.Add("Florida");
+            VacationSpots.Add("Mexico");
+
         }
     }
+
 }
---
+++ src/ChinookSolution/WebApp/Pages/SamplePages/SelectionList.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Pages.SamplePages
+{
+    public class SelectionList
+    {
+        public int ValueID { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
