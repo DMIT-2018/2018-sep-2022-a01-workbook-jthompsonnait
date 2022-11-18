29dd673ffb5d87eaa5e4c5d703005c703c0ae285
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-11-18 09:26:38 -0700

    Setting Index webpage for project

---
+++ src/ChinookSolution/WebApp/Pages/MyProject/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.SamplePages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
