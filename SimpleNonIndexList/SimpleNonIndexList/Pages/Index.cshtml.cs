4b7417b44e8ba15d2d3be0f19e7f8e25f2a8074b
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2022-12-01 07:11:41 -0700

    Simple Non Index List
    Providing a simple example of a list that does not have a primary key but has a index key build on the fly

---
+++ SimpleNonIndexList/SimpleNonIndexList/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleNonIndexList.ViewModel;
+
+namespace SimpleNonIndexList.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty] public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        [BindProperty] public int RemoveEmployeeID { get; set; }
+
+        [BindProperty] public string EmployeeName { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            //Employees.Add(new Employee() { EmployeeId = 1, Name = "James" });
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostRemoveEmployee()
+        {
+            var selectedItem = Employees.SingleOrDefault(x => x.EmployeeId == RemoveEmployeeID);
+            if (selectedItem != null)
+            {
+                Employees.Remove(selectedItem);
+            }
+        }
+
+        public void OnPostAddToEmployeeList()
+        {
+            int maxId = Employees.Count == 0
+                ? 1
+                : Employees.OrderBy(x => x.EmployeeId)
+                    .Max(x => x.EmployeeId) + 1;
+            Employees.Add(new Employee() { EmployeeId = maxId, Name = EmployeeName });
+            
+        }
+    }
+}
---
+++ SimpleNonIndexList/SimpleNonIndexList/ViewModel/Employee.cs
@@ -0,0 +1,32 @@
+﻿// ***********************************************************************
+// Assembly         : SimpleNonIndexList
+// Author           : James Thompson
+// Created          : 11-30-2022
+//
+// Last Modified By : James Thompson
+// Last Modified On : 11-30-2022
+// ***********************************************************************
+// <copyright file="Employee.cs" company="SimpleNonIndexList">
+//     Copyright (c) NAIT. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace SimpleNonIndexList.ViewModel
+{
+    /// <summary>
+    /// Class Employee.
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets the employee identifier.
+        /// </summary>
+        /// <value>The employee identifier.</value>
+        public int EmployeeId { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+    }
+}
